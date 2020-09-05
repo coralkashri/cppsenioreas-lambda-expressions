f524fa627bac3609c259f7e60bfda9616d6b33db
Korel Kashri <korelkashri@gmail.com>
2020-09-05 21:02:50 +0300

    Initialize Project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+#if __cplusplus <= 201103L // C++11
+
+#elif __cplusplus > 201103L && __cplusplus <= 201402L // C++14
+
+#elif __cplusplus > 201402L && __cplusplus <= 201703L // C++17
+
+#else // C++20
+
+#endif
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+#if __cplusplus <= 201103L // C++11
+
+#elif __cplusplus > 201103L && __cplusplus <= 201402L // C++14
+
+#elif __cplusplus > 201402L && __cplusplus <= 201703L // C++17
+
+#else // C++20
+
+#endif
+
+    return EXIT_SUCCESS;
 }
